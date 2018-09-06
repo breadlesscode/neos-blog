e5f66555b3d75e394e2ef83cc8f697fd2b625773
Marvin Kuhn <marvin.kuhn@noerdisch.de>
2018-09-06 16:47:15 +0200

    added the add comment form finisher and added some functions to the comment service

---
+++ Classes/Form/Finisher/AddCommentFormFinisher.php
@@ -0,0 +1,46 @@
+<?php
+namespace Breadlesscode\Blog\Form\Finisher;
+
+use Breadlesscode\Blog\Dto\CommentDto;
+use Breadlesscode\Blog\Exception\InvalidNodeTypeException;
+use Breadlesscode\Blog\Service\CommentService;
+use Neos\Form\Core\Model\AbstractFinisher;
+use Neos\Flow\Annotations as Flow;
+
+class AddCommentFormFinisher extends AbstractFinisher
+{
+    /**
+     * @var CommentService
+     * @Flow\Inject()
+     */
+    protected $commentService;
+
+    /**
+     * this finisher adds a comment to a given node
+     */
+    protected function executeInternal()
+    {
+        try {
+            $commentableNodeIdentifier = $this->parseOption('commentable_node');
+            $commentableNode = $this->commentService->findCommentable($commentableNodeIdentifier);
+            $commentNode = $this->commentService->createNode($this->getCommentDto());
+
+            $this->commentService->addToPost($commentableNode, $commentNode);
+        } catch (\Exception | InvalidNodeTypeException $exception) {
+            $this->finisherContext->cancel();
+        }
+    }
+
+    /**
+     * @return CommentDto
+     */
+    protected function getCommentDto(): CommentDto
+    {
+        $comment = new CommentDto();
+        $comment->setName($this->parseOption('name'));
+        $comment->setEmail($this->parseOption('email'));
+        $comment->setContent($this->parseOption('content'));
+
+        return $comment;
+    }
+}
--- Classes/Service/CommentService.php
+++ Classes/Service/CommentService.php
@@ -2,10 +2,23 @@
 namespace Breadlesscode\Blog\Service;
 
 use Breadlesscode\Blog\Dto\CommentDto;
+use Breadlesscode\Blog\Exception\InvalidNodeTypeException;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
 
 class CommentService
 {
+    /**
+     * node type of the commentable mixin
+     */
+    const COMMENTABLE_NODE_TYPE = 'Breadlesscode.Blog:Mixin.Commentable';
+
+    /**
+     * @Flow\Inject
+     * @var ContextFactoryInterface
+     */
+    private $contextFactory;
+
     /**
      * creates a comment node from the CommentDto
      *
@@ -22,12 +35,50 @@ class CommentService
     /**
      * adds a comment node to a blog post node
      *
-     * @param NodeInterface $blogPostNode
+     * @param NodeInterface $commentableNode
      * @param NodeInterface $commentNode
+     * @throws InvalidNodeTypeException
      */
-    public function addToPost(NodeInterface $blogPostNode, NodeInterface $commentNode)
+    public function addToPost(NodeInterface $commentableNode, NodeInterface $commentNode)
     {
-        $commentCollection = $blogPostNode->getNode('comments');
+        if (!$commentableNode->getNodeType()->isOfType(self::COMMENTABLE_NODE_TYPE)) {
+            throw new InvalidNodeTypeException('The commentable node type should implement the mixin '.self::COMMENTABLE_NODE_TYPE, 1536244558);
+        }
+
+        $commentCollection = $commentableNode->getNode('comments');
         $commentCollection->moveInto($commentNode);
     }
+
+    /**
+     * @param string $commentableIdentifier
+     * @return NodeInterface|null
+     * @throws \Exception
+     */
+    public function findCommentable(string $commentableIdentifier): ?NodeInterface
+    {
+        $commentableNode = $this->getContext()->getNodeByIdentifier($commentableIdentifier);
+
+        if (!$commentableNode->getNodeType()->isOfType(self::COMMENTABLE_NODE_TYPE)) {
+            throw new InvalidNodeTypeException('The commentable node type should implement the mixin '.self::COMMENTABLE_NODE_TYPE, 1536244558);
+        }
+
+        return $commentableNode;
+    }
+
+    /**
+     * @return \Neos\ContentRepository\Domain\Service\Context
+     * @throws \Exception
+     */
+    protected function getContext()
+    {
+        return $this->contextFactory->create([
+            'workspaceName' => 'live',
+            'currentDateTime' => new \Neos\Flow\Utility\Now(),
+            'dimensions' => [],
+            'invisibleContentShown' => true,
+            'removedContentShown' => false,
+            'inaccessibleContentShown' => false
+        ]);
+    }
+
 }
